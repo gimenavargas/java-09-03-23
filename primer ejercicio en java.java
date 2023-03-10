d9de8c380970b8bf20eabb12f61baede0fac0aa7
gimenavargas <1471366@senati.pe>
2023-03-10 07:42:40 -0500

    java 2 clase

---
+++ primer ejercicio en java.java
@@ -0,0 +1,14 @@
+package com.microsoft.java;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App
+{
+    public static void main(String[]args)
+    {
+        System out println( "Hello World!" );
+    }
+}
+ 
